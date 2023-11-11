a19d89d47daf4517dc15d90a58ff9b571403e420
Siddharth Ranjan <siddharthranjan0909@gmail.com>
2023-11-11 16:19:44 +0530

    day 2..

---
+++ src/main/java/com/springcore/collections/test.java
@@ -0,0 +1,22 @@
+package com.springcore.collections;
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class test {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("collectionConfig.xml");
+        Emp emp1 = (Emp) context.getBean("emp1");
+
+        System.out.println(emp1.getName());
+        System.out.println(emp1.getPhones());
+        System.out.println(emp1.getAddresses());
+        System.out.println(emp1.getCourses());
+        System.out.println(emp1.getProps());
+
+        System.out.println(emp1);
+    }
+}
--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -15,6 +15,16 @@ public class App
         ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
         Student student1 = (Student) context.getBean("student1");
 
+        Student student2 = (Student) context.getBean("student2");
+
+        Student student3 = (Student) context.getBean("student3");
+
+        Student student4 = (Student) context.getBean("student4");
+
         System.out.println(student1);
+        System.out.println(student2);
+        System.out.println(student3);
+        System.out.println(student4);
+
     }
 }
