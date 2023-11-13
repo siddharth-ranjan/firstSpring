e35df56dec19ecb60933bc9c59d8b14298a06c35
Siddharth Ranjan <siddharthranjan0909@gmail.com>
2023-11-13 12:14:37 +0530

    day 2-3..

---
+++ src/main/java/autowire/test.java
@@ -0,0 +1,13 @@
+package autowire;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class test {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("autoConfig.xml");
+        emp e1 = context.getBean("e1", emp.class);
+
+        System.out.println(e1);
+    }
+}
